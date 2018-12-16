1650d4347279688b1a1beed7863aaddf13ef8779
Treeston <treeston.mmoc@gmail.com>
2018-12-16 17:14:44 +0100

    get rid of overcomplicated expand animation stuff for toolbox

--- toolbox.js
+++ toolbox.js
@@ -1,27 +1,8 @@
 document.addEventListener("DOMContentLoaded",function()
 {
     document.getElementById('toolbox-settings').addEventListener("click", function() { ShowModal('modal-settings'); });
-    document.getElementById('toolbox-main').addEventListener("mouseover", function()
-    {
-        if (!this.style.height)
-            this.style.height = document.getElementById('toolbox-main-container').clientHeight + 'px';
-        document.getElementById('toolbox-main-export').style.display = '';
-    });
-    document.getElementById('toolbox-export').addEventListener("click", function()
-    {
-        var parent = document.getElementById('toolbox-main');
-        var sibling = document.getElementById('toolbox-main-container');
-        var child = document.getElementById('toolbox-main-export');
-        if (child.expanded)
-        {
-            child.expanded = false;
-            parent.style.height = sibling.clientHeight + 'px';
-        }
-        else
-        {
-            child.expanded = true;
-            parent.style.height = (sibling.clientHeight + child.clientHeight + 7) + 'px';
-        }
-    });
+    document.getElementById('toolbox-copyurl').addEventListener("click", function() { alert('NYI'); });
     document.getElementById('toolbox-close').addEventListener("click", function() { document.location.hash = ''; ReloadFromHashData(); });
+    document.getElementById('toolbox-export-ydk').addEventListener("click", function() { alert('NYI'); });
+    document.getElementById('toolbox-export-text').addEventListener("click", function() { alert('NYI'); });
 });
